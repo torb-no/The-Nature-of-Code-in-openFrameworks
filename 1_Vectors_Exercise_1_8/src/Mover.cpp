3e41fc897bb0477b167acd28eb32fac94e138f63
Torbjørn Vik Lunde <post@torbjornlunde.com>
2014-01-22 21:11:44 +0100

    Initial (same multiplication regardless of speed)

---
+++ 1_Vectors_Exercise_1_8/src/Mover.cpp
@@ -0,0 +1,37 @@
+//
+//  Mover.cpp
+//  1_Vectors_Exercise_1_8
+//
+//  Created by Torbjørn on 22.01.14.
+//
+//
+
+#include "Mover.h"
+
+Mover::Mover() {
+    location.x = ofGetWidth()/2;
+    location.y = ofGetHeight()/2;
+    
+    velocity.x = 0;
+    velocity.y = 0;
+    
+    acceleration.x = 0;
+    acceleration.y = 0;
+}
+
+void Mover::update() {
+    ofVec2f mouse;
+    mouse.x = parent->mouseX;
+    mouse.y = parent->mouseY;
+    ofVec2f direction = mouse - location;
+    direction.normalize();
+    direction *= 0.25;
+    acceleration = direction;
+    
+    velocity = velocity + acceleration;
+    location = location + velocity;
+}
+
+void Mover::draw() {
+    ofCircle(location, 20);
+}
---
+++ 1_Vectors_Exercise_1_8/src/Mover.h
@@ -0,0 +1,29 @@
+//
+//  Mover.h
+//  1_Vectors_Exercise_1_8
+//
+//  Created by Torbjørn on 22.01.14.
+//
+//
+
+#ifndef ____Vectors_Exercise_1_8__Mover__
+#define ____Vectors_Exercise_1_8__Mover__
+
+#include <iostream>
+#include "ofMain.h"
+
+class Mover {
+    
+public:
+    Mover();
+    void update();
+    void draw();
+    
+    ofVec2f location;
+    ofVec2f velocity;
+    ofVec2f acceleration;
+    
+    ofBaseApp* parent;
+};
+
+#endif /* defined(____Vectors_Exercise_1_8__Mover__) */
