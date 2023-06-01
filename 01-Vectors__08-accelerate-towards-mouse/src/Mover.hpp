bd924159131a6894aa798327c43e9f07776230c6
Torb Lunde Miranda <1179046+torb-no@users.noreply.github.com>
2023-06-01 18:16:32 +0200

    Merged class into single file

--- 01-Vectors__08-accelerate-towards-mouse/src/Mover.hpp
+++ 01-Vectors__08-accelerate-towards-mouse/src/Mover.hpp
@@ -19,10 +19,36 @@ class Mover {
     glm::vec2 mouse;
     
 public:
-    Mover();
-    void update();
-    void draw();
-    void mouseMoved(int x, int y);
+    Mover() {
+        position.x = ofGetWidth() / 2;
+        position.y = ofGetHeight() / 2;
+    }
+    
+    void update() {
+        // Acceleration towards mouse
+        auto difference = mouse - position;
+        auto distance = glm::length(difference);
+        auto direction = glm::normalize(difference);
+        auto accelerationMagnitude = glm::clamp(distance/2000.f, 0.f, 2.f);
+        acceleration = direction * accelerationMagnitude;
+        
+        velocity += acceleration;
+        constexpr auto maxVelocity = 15.f;
+        if (glm::length(velocity) > maxVelocity) {
+            velocity = glm::normalize(velocity) * maxVelocity;
+        }
+        
+        position += velocity;
+    }
+    
+    void draw() {
+        ofDrawCircle(position, 20);
+    }
+    
+    void mouseMoved(int x, int y) {
+        mouse.x = (float) x;
+        mouse.y = (float) y;
+    }
 };
 
 #endif /* Mover_hpp */
